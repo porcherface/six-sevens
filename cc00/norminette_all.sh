826004cf7fe2062cc6e84791536bbab2fff798e0
amazzei <amazzei@student.42roma.it>
2020-11-26 20:39:56 +0100

    added norminette test routine

--- cc00/norminette_all.sh
+++ cc00/norminette_all.sh
@@ -1,1 +1,38 @@
 # norminette all
+cd code 
+
+cd ex00
+Norminette ft_putchar.c
+cd ..
+
+cd ex01
+Norminette ft_print_alphabet.c
+cd ..
+
+cd ex02
+Norminette ft_print_reverse_alphabet.c
+cd ..
+
+cd ex03
+Norminette ft_print_numbers.c
+cd ..
+
+cd ex04
+Norminette ft_is_negative.c
+cd ..
+
+cd ex05
+Norminette ft_print_comb.c
+cd ..
+
+cd ex06
+Norminette ft_print_comb2.c
+cd ..
+
+cd ex07
+Norminette ft_putnbr.c
+cd ..
+
+cd ex08
+Norminette ft_print_combn.c
+cd ..
