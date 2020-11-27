4233cd21f842ac092e5b0bb270fe657341609fb7
Andrea Mazzei <mazzei@localhost.localdomain>
2020-11-27 03:02:17 +0100

    tired, too tired

--- cc00/code/ex08/ft_print_combn.c
+++ cc00/code/ex08/ft_print_combn.c
@@ -12,66 +12,95 @@
 
 #include	<unistd.h>
 
-void	init_array(char arr[10], int n)
+void	init_array(int *arr, int n)
 {
 	int iterator;
 
-	iterator = 0
-	arr[0] = 0;
+	iterator = 0;
 	while (iterator < n)
 	{
 		arr[iterator] = iterator;
 		iterator++;
-	}
+	}	
 	return ;
 }
 
-void	update_array(char arr[10], int n)
+void	update_array(int *arr, int n)
 {
-	int iterator;
-
-	iterator = n;
-	if (arr[iterator] != iterator) 
+	
+	if (arr[n - 1] != 10 - n) 
 	{
-		arr[iterator]++;
+		arr[n - 1]++;
 	} 
 	else
 	{
-		update_array(arr, n-1);
+		if (n -1 > 0)
+		{		
+			arr[n - 1] = arr[n - 2]+1;
+			update_array(arr, n-1);
+		}
+		else
+		{
+			arr[n - 1] = 0;
+		}	
 	}
 	return ;
 }
 
-int	terminate(char arr[10], int n)
+int	terminate(int arr[10], int n)
 {
 	int iterator;
 	int terminate;
+	int counter;
 
 	terminate = 1;
-	iterator = 0
-	while (iterator < n)
+	iterator = n-1;
+	counter = 0;
+	while (iterator >= 0)
 	{
-		if (arr[iterator] != iterator);
-		{
+		if (arr[iterator] != 9 - counter)
+		{	
 			terminate = 0;
 		}
 		iterator--;
+		counter++;
 	}
 	return (terminate);
 }
 
+void	write_ctoa(int arr[10], int n)
+{
+	int iterator;
+	char c;
+
+	iterator = 0;
+	while (iterator<n)
+	{
+		c = arr[iterator] + '0';
+		write(1, &c, 1);
+		iterator++;
+	}
+
+}
+
 void	ft_print_combn(int n)
 {
-	char arr[10];
+	int arr[10];
+	int *arrp;
 
-	init_array(arr, n);
-	while(!terminate(arr, n))
+	arrp = arr;
+	if (n <= 10 && n >= 1)
 	{
-		update_array(arr,n);
-		write(1, arr, n);
-		write(1, ", ", 2);
+		init_array(arrp, n);
+		while (!terminate(arr, n))
+		{
+			write_ctoa(arr, n);
+			write(1, ", ", 2);
+			update_array(arrp,n);
+
+		}
+		write(1, arrp, n);
 	}
-	write(1, arr, n);
 	return ;
 }
 
