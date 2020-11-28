ba4db5cb048c40552b51858f991aff5cd2f549a2
amazzei <amazzei@student.42roma.it>
2020-11-28 11:26:18 +0100

    started rooting project

---
+++ crush00/compile_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd code
+main=main.c
+oracle=c00_oracle.txt
+
+#ls ex0*/*
+others=""
+others=$others"code/ft_putchar.c rushXX.c "
+
+Norminette $main $others -R CheckForbiddenSourceHeader
+gcc $main $others -Wall -Wextra -Werror -o c00_test.out
+
+./c00_test.out > $oracle
