63b7ca02d08dad2c6f6e2cbb2d67ef68f075ac9e
Andrea Mazzei <mazzei@localhost.localdomain>
2020-11-29 13:13:51 +0100

    upperoneoneone

--- crush00/compile_run.sh
+++ crush00/compile_run.sh
@@ -6,30 +6,35 @@ main=code4/main.c
 #ls ex0*/*
 
 name=rush00
+echo running $name...
 others="code4/ft_putchar.c code4/$name.c"
 #Norminette $main $others -R CheckForbiddenSourceHeader
 gcc $main $others -Wall -Wextra -Werror -o builds/$name".out"
 ./builds/$name".out" #> builds/$name".log"
 
 name=rush01
+echo running $name...
 others="code4/ft_putchar.c code4/rush01.c "
 #Norminette $main $others -R CheckForbiddenSourceHeader
 gcc $main $others -Wall -Wextra -Werror -o builds/$name".out"
 ./builds/$name".out" #> builds/$name".log"
 
 name=rush02
+echo running $name...
 others="code4/ft_putchar.c code4/rush02.c "
 #Norminette $main $others -R CheckForbiddenSourceHeader
 gcc $main $others -Wall -Wextra -Werror -o builds/$name".out"
 ./builds/$name".out" #> builds/$name".log"
 
 name=rush03
+echo running $name...
 others="code4/ft_putchar.c code4/rush03.c "
 #Norminette $main $others -R CheckForbiddenSourceHeader
 gcc $main $others -Wall -Wextra -Werror -o builds/$name".out"
 ./builds/$name".out" 
 
 name=rush04
+echo running $name...
 others="code4/ft_putchar.c code4/rush04.c "
 #Norminette $main $others -R CheckForbiddenSourceHeader
 gcc $main $others -Wall -Wextra -Werror -o builds/$name".out"
