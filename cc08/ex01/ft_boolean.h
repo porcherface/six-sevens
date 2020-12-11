f40261abde629cfdb88f85f2554081fba1c434a1
amazzei <amazzei@student.42roma.it>
2020-12-11 15:58:43 +0100

    aheiuhhaeiuaegaiuehfiuaeghfuyaefiueaevhiuesvgesvgiuaevfgiuef

--- cc08/ex01/ft_boolean.h
+++ cc08/ex01/ft_boolean.h
@@ -1,21 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amazzei <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 13:42:13 by amazzei           #+#    #+#             */
+/*   Updated: 2020/12/11 13:42:16 by amazzei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_BOOLEAN_H
-#define FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
-#define EVEN_MSG ("I have an even number of arguments.")
-#define EVEN_MSG ("I have an odd number of arguments.")
+# include <unistd.h>
+# define EVEN_MSG	"I have an even number of arguments.\n"
+# define ODD_MSG	"I have an odd number of arguments.\n"
+# define SUCCESS	0
+# define TRUE		1
+# define FALSE		0
+# define EVEN(nbr)	(!(nbr % 2))
+typedef int t_bool;
 
-#define SUCCESS	(0)
-/**
-
-return ((EVEN(nbr)) ? TRUE : FALSE);
-}
-int main(int argc, char **argv)
-{
-(void)argv;
-if (ft_is_even(argc - 1) == TRUE)
-ft_putstr(EVEN_MSG);
-else
-ft_putstr(ODD_MSG);
-return (SUCCESS);
-}*/
 #endif
--- cc08/ex04/ft_strs_to_tab.c
+++ cc08/ex04/ft_strs_to_tab.c
@@ -1,16 +1,70 @@
-#include "ft_stock_str.h" 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amazzei <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 15:29:54 by amazzei           #+#    #+#             */
+/*   Updated: 2020/12/11 15:29:56 by amazzei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-/* proto
-struct s_stock_str *ft_strs_to_tab(int ac, char **av);
+#include "ft_stock_str.h"
 
-*//* .h file  
-
-typedef struct s_stock_str
+int		ft_strlen_c8(char *str)
 {
-int size;
-char *str;
-char *copy;
-} t_stock_str;
- .h file  */
+	int it;
+
+	it = 0;
+	while (str[it] != '\0')
+	{
+		it++;
+	}
+	return (it);
+}
+
+void	ft_strvcpy(char *dest, char *src)
+{
+	int it;
+
+	it = 0;
+	while (src[it] != '\0')
+	{
+		dest[it] = src[it];
+		it++;
+	}
+	if (src[it] == '\0')
+		dest[it] = src[it];
+	return ;
+}
 
 
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str		*out;
+	unsigned int	it;
+	
+	it = 0;
+	if (ac)
+		out = (t_stock_str *)malloc(ac * sizeof(t_stock_str));
+	while (it < size)
+	{
+		out[it].size = ft_strlen_c8(av[it]);
+		if (out[it].size)
+		{
+			out[it].str = (char *)malloc((1 + out[it].size) * sizeof(char));
+			out[it].copy = (char *)malloc((1 + out[it].size) * sizeof(char));
+		}
+		if(!out[it].str)
+			return (0x0);
+		else
+			ft_strlcpy(out[it].str, av[it]);
+		if(!out[it].copy)
+			return (0x0);
+		else
+			ft_strlcpy(out[it].str, out[it].copy);	
+		it++;
+	}
+	return (out);
+}
